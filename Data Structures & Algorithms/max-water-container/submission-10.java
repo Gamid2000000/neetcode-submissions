db33126667e6889673f2b506543d844278a3c3eb
Gamid Aliev <Gamidaliev12@gmail.com>
2026-07-04 18:27:49 +0300

    Add: max-water-container - submission-10

---
+++ Data Structures & Algorithms/max-water-container/submission-10.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] h) {
+      int left = 0;
+      int right = h.length-1;
+      int area = 0;
+
+      while(left<=right){
+        area = Math.max(area, Math.min(h[left], h[right]) * (right - left));
+
+        if(h[left] < h[right]){
+          left++;
+        }
+        else{
+          right--;
+        }
+      }
+      return area;
+    }
+}
