3ecb9df6ce15bdc343dcce6ae5c2aafd0d09f763
Gamid Aliev <Gamidaliev12@gmail.com>
2026-07-08 19:52:57 +0300

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+        int res = right;
+
+        while(left<=right){
+            int k = left + (right-left) /2;
+            int total = 0; 
+
+            for(int p: piles){
+                total += Math.ceil((double) p/k);
+            }
+            if(total <= h){
+                res = k;
+                right = k-1;
+            }
+            else{
+                left = k+1;
+            }
+        }
+        return res;
+    }
+}
